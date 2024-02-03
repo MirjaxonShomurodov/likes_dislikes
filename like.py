aa58f2a0f40b2c44d5b13b0e3bc19c861296f0bb
MirjaxonShomurodov <mirjaxon457@gmail.com>
2024-02-03 08:32:47 +0500

    like.py

--- like.py
+++ like.py
@@ -5,8 +5,11 @@ TOKEN = '6839845710:AAE41G3cQslmBNL8FYMXwDFtqgUnoG_Uy3U'
 
 bot = telegram.Bot(TOKEN)
 
-@app.route('/')
+@app.route('/',methods = ['POST'])
 def index():
+    data = request.get_json()
+    chat_id = data['message']['chat']['id']
+    bot.send_message(chat_id=chat_id,text = data['message']['text'])
     return "Har doimgdek, 'Hello world'"
 
 if __name__=="__main__":
