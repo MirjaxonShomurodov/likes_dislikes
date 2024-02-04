1e6a3055ec70dba68a22e23b58ec8c49d27e60c1
MirjaxonShomurodov <mirjaxon457@gmail.com>
2024-02-04 14:58:28 +0500

    likes

--- like.py
+++ like.py
@@ -1,19 +1,46 @@
 TOKEN = "6839845710:AAE41G3cQslmBNL8FYMXwDFtqgUnoG_Uy3U"
-from telegram import Update
+from telegram.ext import CallbackContext
 from keyboards import keyboard,inline_keyboard
+from telegram import Update
 from flask import Flask,request
 import telegram
-from db import inc_dislike,inc_like,get_user,clear,add_user,inc_inline_dislike,inc_inline_like,inline_clear
+from db import inc_dislike,inc_like,get_user,clear,add_user,inc_inline_dislike,inc_inline_like,inline_clear,is_user
 app = Flask(__name__)
 
 bot = telegram.Bot(TOKEN)
+def start(update: Update, context: CallbackContext):
+    user = update.effective_user
+    if not is_user(chat_id=str(user.id)):
+        add_user(chat_id=str(user.id))
+        update.message.reply_html(
+            text=f'Hello, {user.full_name}! Press one of the buttons.',
+            reply_markup=keyboard
+        )
+        update.message.reply_html(
+            text=f'Hello, {user.full_name}! Press one of the buttons.',
+            reply_markup=inline_keyboard
+        )
+    else:
+        user_data = get_user(chat_id=str(user.id))
+        update.message.reply_html(
+            text=f'<b>inline likes:</b> {user_data["inline_likes"]}\n<b>inline dislikes:</b> {user_data["inline_dislikes"]}',
+            reply_markup=keyboard
+        )
+        update.message.reply_html(
+            text=f'<b>inline likes:</b> {user_data["inline_likes"]}\n<b>inline dislikes:</b> {user_data["inline_dislikes"]}',
+            reply_markup=inline_keyboard
+        )
 
 @app.route('/',methods = ['POST'])
 def index():  
     data = request.get_json()
     chat_ids = data['message']['from']['id']
     print(data)
+
     if data['message']['text']=='/start':
+        if not is_user(chat_id=str(chat_ids)):
+            start(user=data['message']['chat'])
+
         add_user(chat_id=str(chat_ids))
         bot.send_message(
             chat_id=chat_ids,
@@ -21,6 +48,9 @@ def index():
             reply_markup=keyboard
         )
     elif data['message']['text']=='👍':
+        if not is_user(str(chat_ids)):
+            start(user = data['message']['chat'])
+
         inc_like(chat_id=str(chat_ids))
         user_data = get_user(chat_id=str(chat_ids))
         bot.send_message(
@@ -29,6 +59,9 @@ def index():
             reply_markup=keyboard
         )
     elif data['message']['text']=='👎':
+        if not is_user(str(chat_ids)):
+            start(user = data['message']['chat'])
+
         inc_dislike(chat_id=str(chat_ids))
         user_data = get_user(chat_id=str(chat_ids))
         bot.send_message(
@@ -44,7 +77,10 @@ def index():
             text=f'likes:{user_data["likes"]}\ndislikes: {user_data["dislikes"]}',
             reply_markup=keyboard
         )
-    elif data['message']['text']!=None:
+    elif data['message'].get('photo')!=None:
+        if not is_user(str(chat_ids)):
+            start(user = data['message']['chat'])
+
         inc_inline_like(chat_id=str(chat_ids))
         user_data =get_user(chat_id=str(chat_ids))
         bot.send_message(
@@ -52,7 +88,10 @@ def index():
             text=f'likes:{user_data["inline_likes"]}\ndislikes: {user_data["dislikes"]}',
             reply_markup=keyboard
         )
-    elif data['message'].get('photo')!=None:
+    elif data['message'].get('photo')!=None:        
+        if not is_user(str(chat_ids)):
+            start(user = data['message']['chat'])
+
         inc_inline_dislike(chat_id=str(chat_ids))
         user_data = get_user(chat_id=str(chat_ids))
         bot.send_message(
@@ -60,10 +99,13 @@ def index():
             text = f'inline_likes:{user_data["inline_likes"]}\ninline_dislikes: {user_data["inline_dislikes"]}',
             reply_markup=inline_keyboard
         )
-    elif data['message'].get('photo')!=None:
+    elif data['message'].get('photo')!=None:        
+        if not is_user(str(chat_ids)):
+            start(user = data['message']['chat'])
+            
         inline_clear(chat_id=str(chat_ids))
         user_data = get_user(chat_id=chat_ids)
-        bot.send_message(
+        bot.edit_message_text(
             chat_id = chat_ids,
             text = f'inline_likes:{user_data["inline_likes"]}\ninline_dislikes: {user_data["inline_dislikes"]}',
             reply_markup=inline_keyboard
